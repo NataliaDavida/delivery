539f9fa8dfc5b07cae4e9df3ffd78b16d06131e9
NataliaDavida <nataliadavida88@gmail.com>
2022-07-12 07:02:57 +0300

    delivery1

--- shop-app/components/Shopping/Shopping.js
+++ shop-app/components/Shopping/Shopping.js
@@ -30,6 +30,24 @@ class Shopping {
                         <td class="shopping-element__price">${sumCatalog.toLocaleString()} UAN</td>
                     </tr>
                 </table>
+                <form action="/action_page.php">
+                <h1>Delivery data</h1>
+                    <fieldset>
+                    <legend></legend>
+                    <label for="fname">First name:</label>
+                    <input type="text" id="fname" name="fname"><br>
+                    <label for="lname">Last name:</label>
+                    <input type="text" id="lname" name="lname"><br>
+                    <label for="phone">Phone number:</label>
+                    <input type="tel" id="phone" name="phone" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"><br>
+                    <label for="email">Email:</label>
+                    <input type="email" id="email" name="email"><br>
+                    <label for="adress">Adress:</label>
+                    <input type="text" id="adress" name="adress"><br>
+    
+                    <button type="submit" class="btn">Submit</button>
+                    </fieldset>
+                    </form>
             </div>
         `;
 
